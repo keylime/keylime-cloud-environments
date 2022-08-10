46d9946738939ba56f5d4cdb18607eed58045451
Avery Blanchard <ablancha@redhat.com>
2022-08-10 11:56:25 -0400

    Add ansible role for AWS
    Signed-off-by: Avery Blanchard <ablancha@redhat.com>

---
+++ keylime-ansible-aws/set_env_var.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+usage() {
+        echo "Usage: ./set_env_var.sh <AWS access key ID> <AWS secret key> <AWS region>"
+}
+if [[ ($# -ne 3) || ($@ == "--help") || ($@ == "-h") ]]
+then
+        usage
+        exit 1
+fi
+export AWS_ACCESS_KEY="$1"
+echo "AWS_ACCESS_KEY is set to $AWS_ACCESS_KEY"
+export AWS_SECRET_KEY="$2"
+echo "AWS_SECRET_KEY is set to $AWS_SECRET_KEY"
+export AWS_REGION="$3"
+echo "AWS_REGION is set to $AWS_REGION"
+$SHELL
